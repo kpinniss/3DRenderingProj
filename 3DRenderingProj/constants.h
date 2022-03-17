b37daa95b3068c1b959dd1fde1ba3abbf00527a0
Kristoffer Inniss <kpinniss@gmail.com>
2022-03-16 18:06:41 -0700

    Add project files.

---
+++ 3DRenderingProj/constants.h
@@ -0,0 +1,13 @@
+#ifndef CONSTANTS_H
+
+#define CONSTANTS_H
+#define FALSE 0
+#define TRUE 1
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+#define DEFAULT_INDEX -1
+#define FPS 60
+#define FRAME_TARGET_TIME (1000/FPS)
+#define FLOAT_DIV 1000.0f
+
+#endif
